22ba6746fdfa03543bb6f00ccb745350633b1cc3
MonicaMartz <monik92_ms@hotmail.com>
2019-11-13 13:07:37 -0600

    filtrado

--- src/data.js
+++ src/data.js
@@ -2,6 +2,15 @@
 
 // esta es una función de ejemplo
 
-export const example = () => {
+/*export const filterHouse = () => {
+  let house= POTTER.filter()
+
   return 'example';
-};
+};*/
+
+export const filterHouse = (POTTER) =>{
+  const house = POTTER.filter(personage => (personage.house === "Gryffindor"));
+  return house;
+}
+
+//console.assert(POTTER["house"]);
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-/**
+ /**
  * import POKEMON from './data/pokemon/pokemon.js'
  * import LoL from './data/lol/lol.js'
  * import POTTER from './data/potter/potter.js'
  */
 
-import { example } from './data.js';
+import { POTTER } from './data/potter/potter.js';
+import { filterHouse } from './data.js';
 
-console.log(example);
+console.log(filterHouse(POTTER));
 
 /*
  * console.log(POKEMON);
