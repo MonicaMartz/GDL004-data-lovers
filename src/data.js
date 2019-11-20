a75d161bfb47236c33b60bbf1e0c34265f8a9b67
MonicaMartz <monik92_ms@hotmail.com>
2019-11-20 00:11:40 -0600

    Estructura de 3 primeras ventanas, registro de usuario en javascript y borrador de filtro

--- src/data.js
+++ src/data.js
@@ -10,7 +10,32 @@
 
 export const filterHouse = (POTTER) =>{
   const house = POTTER.filter(personage => (personage.house === "Gryffindor"));
+
   return house;
 }
 
+/*export const filterHouse = (POTTER) =>{
+  const house = POTTER.filter(personage => (personage.house === "Gryffindor"));
+
+
+  return house;
+}
+*/
+/*export const filterHouse = (POTTER) => {
+  for(let i=0; i<POTTER.length; i++){
+    if(POTTER[i].house =="Griffindor")
+    let casa += (POTTER[i].name);
+    return casa;
+  }
+}
+/*export const filterHouse = (POTTER) =>{
+  for(let i=; i<POTTER.length; i++){
+
+  }
+  const house = POTTER.filter(personage => (personage.house === "Gryffindor"));
+  return house;
+}*/
+
+
+/*export const NameHouse = POTTER.map(personage )*/
 //console.assert(POTTER["house"]);
--- src/main.js
+++ src/main.js
@@ -4,10 +4,38 @@
  * import POTTER from './data/potter/potter.js'
  */
 
+/*REGISTRA Y MUESTRA EL NOMBRE DEL USUARIO*/
+function welcomeUser() {
+    let welcome = document.getElementById("name").value;
+    document.getElementById("user").innerHTML= welcome;
+
+   console.log(welcome);
+}
+document.getElementById("boton").addEventListener("click",welcomeUser);
+
+/*Cambio de pagina 1 a 2*/
+
+function cambioPagina() {
+    document.getElementById('pagina2').style.display = "block";
+    document.getElementById('pagina1').style.display = "none";
+}
+
+
+
+
+
 import { POTTER } from './data/potter/potter.js';
+ /*prueba funcion
+function filtrar (){*/
 import { filterHouse } from './data.js';
 
 console.log(filterHouse(POTTER));
+
+
+document.getElementById("PersonGriffindor").innerHTML += (filterHouse(POTTER));
+
+document.getElementById("Griffindor").addEventListener("click",(filterHouse(POTTER)));
+
 
 /*
  * console.log(POKEMON);
